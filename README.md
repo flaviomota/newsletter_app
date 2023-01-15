9184f6e957d98839d2b5b7e0a55bd0ea58234c78
fmota89 <fmota@student.42lisboa.com>
2023-01-15 11:38:16 +0000

    mailchimp integration

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is a simple newsletter app that allows users to sign up for a newsletter and receive emails.
 
+After that I used the MailChimp API to create a new list and add the user to that list.
+
 It's a single page web site, and on the front end it looks incredibly simple, but on the backend it's got some powerful functionality that will allow you to sign people up to your mailing list.
--- app.js
+++ app.js
@@ -5,6 +5,40 @@ const request = require("request");
 const port = 3000;
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        },
+      }
+    ]
+  };
+
+});
+
 app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
-})
+});
+
+// Mailchimp API Key
+// e9d41efe7fbe31fad9398211ea809fe4-us21
+
+// list id
+// 73bf87d946
